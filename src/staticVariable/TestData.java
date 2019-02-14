4a0079e7cdfa07379e74d1a7b720106cd564eed3
kiraz26 <halilkirazz@gmail.com>
2019-02-13 21:10:56 -0500

    1

--- src/staticVariable/TestData.java
+++ src/staticVariable/TestData.java
@@ -2,8 +2,8 @@ package staticVariable;
 
 public class TestData {
 	public static String url="http://etsy.com";
-	public static String userId = "halilkirazz@gmail.com";
-	public static String password = "ASD123asd.";
+	public static String userId = "abc@gmail.com";
+	public static String password = "12345";
 	public static String searchItem = "wooden spoon";
 	public static String gmailUserID = "testUser@gmail.com";
 }
